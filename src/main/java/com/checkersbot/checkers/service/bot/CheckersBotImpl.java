7604acbb4479214d2fd654ab9870f3a0067905b2
HellaxA <abereznikov64@gmail.com>
2020-12-17 07:31:51 +0200

    All nearby moves

--- src/main/java/com/checkersbot/checkers/service/bot/CheckersBotImpl.java
+++ src/main/java/com/checkersbot/checkers/service/bot/CheckersBotImpl.java
@@ -51,24 +51,22 @@ public class CheckersBotImpl implements CheckersBot {
                 if (checker.getColor().equals(BLACK)) {
                     int from = checker.getPosition();
 
-                    List<Checker> opponentsNearBy = findOpponentsNearBy(from, checkers, teamColor);
-
-
+                    list.addAll(findOpponentsNearBy(from, checkers, teamColor));
 
                     if (from - CHECKERS_ON_LINE >= MIN_POSITION) {
                         int to = from - CHECKERS_ON_LINE;
 
-                        checkPosAndAddToList(checkers, teamColor, list, from, to, to);
+                        checkPosAndAddToList(checkers, teamColor, list, from, to);
 
                         if (checker.getRow() % 2 != 0 && checker.getColumn() != 0) {
                             int secondTo = from - CHECKERS_ON_LINE - 1;
 
-                            checkPosAndAddToList(checkers, teamColor, list, from, to, secondTo);
+                            checkPosAndAddToList(checkers, teamColor, list, from, secondTo);
 
                         } else if (checker.getRow() % 2 == 0 && checker.getColumn() != COLS - 1) {
                             int secondTo = from - CHECKERS_ON_LINE + 1;
 
-                            checkPosAndAddToList(checkers, teamColor, list, from, to, secondTo);
+                            checkPosAndAddToList(checkers, teamColor, list, from, secondTo);
                         }
 
                     }
@@ -79,21 +77,21 @@ public class CheckersBotImpl implements CheckersBot {
                 if (checker.getColor().equals(RED)) {
                     int from = checker.getPosition();
 
-                    List<Checker> opponentsNearBy = findOpponentsNearBy(from, checkers, teamColor);
+                    list.addAll(findOpponentsNearBy(from, checkers, teamColor));
 
                     if (from + CHECKERS_ON_LINE <= MAX_POSITION) {
                         int to = from + CHECKERS_ON_LINE;
 
-                        checkPosAndAddToList(checkers, teamColor, list, from, to, to);
+                        checkPosAndAddToList(checkers, teamColor, list, from, to);
 
                         if (checker.getRow() % 2 == 0 && checker.getColumn() != COLS - 1) {
                             int secondTo = from + CHECKERS_ON_LINE + 1;
 
-                            checkPosAndAddToList(checkers, teamColor, list, from, to, secondTo);
+                            checkPosAndAddToList(checkers, teamColor, list, from, secondTo);
                         } else if (checker.getRow() % 2 != 0 && checker.getColumn() != 0) {
                             int secondTo = from + CHECKERS_ON_LINE - 1;
 
-                            checkPosAndAddToList(checkers, teamColor, list, from, to, secondTo);
+                            checkPosAndAddToList(checkers, teamColor, list, from, secondTo);
 
                         }
                     }
@@ -104,8 +102,8 @@ public class CheckersBotImpl implements CheckersBot {
         return list;
     }
 
-    private List<Checker> findOpponentsNearBy(int from, List<Checker> checkers, String teamColor) {
-        List<Checker> positionsNearBy = findPositionsNearBy(
+    private List<Move> findOpponentsNearBy(int from, List<Checker> checkers, String teamColor) {
+        List<Move> positionsNearBy = findPositionsNearBy(
                 Objects.requireNonNull(findCheckerByPosition(from, checkers)), checkers, teamColor
 
         );
@@ -115,8 +113,8 @@ public class CheckersBotImpl implements CheckersBot {
         return positionsNearBy;
     }
 
-    private List<Checker> findPositionsNearBy(Checker checkerFrom, List<Checker> checkers, String teamColor) {
-        List<Checker> res = new ArrayList<>();
+    private List<Move> findPositionsNearBy(Checker checkerFrom, List<Checker> checkers, String teamColor) {
+        List<Move> res = new ArrayList<>();
         int from = checkerFrom.getPosition();
 
         if (checkerFrom.getRow() % 2 == 0) {
@@ -128,26 +126,51 @@ public class CheckersBotImpl implements CheckersBot {
             if (checkerFrom.getRow() != 0 && checkerFrom.getColumn() != 3) {
                 Checker leftDownChecker = findCheckerByPosition(leftDown, checkers);
                 if (leftDownChecker != null && !leftDownChecker.getColor().equals(teamColor)) {
-                    res.add(leftDownChecker);
+                    int destination = leftDownChecker.getPosition() - 4;
+                    Checker tempChecker = findCheckerByPosition(destination, checkers);
+
+                    if (tempChecker == null) {
+                        Move move = new Move(from, destination, 1);
+                        res.add(move);
+                    }
+
                 }
             }
             if (checkerFrom.getRow() != 0 && checkerFrom.getColumn() != 0) {
                 Checker leftUpChecker = findCheckerByPosition(leftUp, checkers);
                 if (leftUpChecker != null && !leftUpChecker.getColor().equals(teamColor)) {
-                    res.add(leftUpChecker);
+                    int destination = leftUpChecker.getPosition() - 5;
+                    Checker tempChecker = findCheckerByPosition(destination, checkers);
+
+                    if (tempChecker == null) {
+                        Move move = new Move(from, destination, 1);
+                        res.add(move);
+                    }
                 }
             }
             if (checkerFrom.getColumn() != 3) {
                 Checker rightDownChecker = findCheckerByPosition(rightDown, checkers);
                 if (rightDownChecker != null && !rightDownChecker.getColor().equals(teamColor)) {
-                    res.add(rightDownChecker);
+                    int destination = rightDownChecker.getPosition() + 4;
+                    Checker tempChecker = findCheckerByPosition(destination, checkers);
+
+                    if (tempChecker == null) {
+                        Move move = new Move(from, destination, 1);
+                        res.add(move);
+                    }
                 }
 
             }
             if (checkerFrom.getColumn() != 0) {
                 Checker rightUpChecker = findCheckerByPosition(rightUp, checkers);
                 if (rightUpChecker != null && !rightUpChecker.getColor().equals(teamColor)) {
-                    res.add(rightUpChecker);
+                    int destination = rightUpChecker.getPosition() + 3;
+                    Checker tempChecker = findCheckerByPosition(destination, checkers);
+
+                    if (tempChecker == null) {
+                        Move move = new Move(from, destination, 1);
+                        res.add(move);
+                    }
                 }
             }
 
@@ -161,26 +184,50 @@ public class CheckersBotImpl implements CheckersBot {
             if (checkerFrom.getColumn() != 3) {
                 Checker leftDownChecker = findCheckerByPosition(leftDown, checkers);
                 if (leftDownChecker != null && !leftDownChecker.getColor().equals(teamColor)) {
-                    res.add(leftDownChecker);
+                    int destination = leftDownChecker.getPosition() - 3;
+                    Checker tempChecker = findCheckerByPosition(destination, checkers);
+
+                    if (tempChecker == null) {
+                        Move move = new Move(from, destination, 1);
+                        res.add(move);
+                    }
                 }
             }
 
             if (checkerFrom.getColumn() != 0 && checkerFrom.getRow() != 1) {
                 Checker leftUpChecker = findCheckerByPosition(leftUp, checkers);
                 if (leftUpChecker != null && !leftUpChecker.getColor().equals(teamColor)) {
-                    res.add(leftUpChecker);
+                    int destination = leftUpChecker.getPosition() - 4;
+                    Checker tempChecker = findCheckerByPosition(destination, checkers);
+
+                    if (tempChecker == null) {
+                        Move move = new Move(from, destination, 1);
+                        res.add(move);
+                    }
                 }
             }
             if (checkerFrom.getRow() != 7 && checkerFrom.getColumn() != 3 && checkerFrom.getRow() != 6) {
                 Checker rightDownChecker = findCheckerByPosition(rightDown, checkers);
                 if (rightDownChecker != null && !rightDownChecker.getColor().equals(teamColor)) {
-                    res.add(rightDownChecker);
+                    int destination = rightDownChecker.getPosition() + 5;
+                    Checker tempChecker = findCheckerByPosition(destination, checkers);
+
+                    if (tempChecker == null) {
+                        Move move = new Move(from, destination, 1);
+                        res.add(move);
+                    }
                 }
             }
             if (checkerFrom.getRow() != 7 && checkerFrom.getColumn() != 0) {
                 Checker rightUpChecker = findCheckerByPosition(rightUp, checkers);
                 if (rightUpChecker != null && !rightUpChecker.getColor().equals(teamColor)) {
-                    res.add(rightUpChecker);
+                    int destination = rightUpChecker.getPosition() + 4;
+                    Checker tempChecker = findCheckerByPosition(destination, checkers);
+
+                    if (tempChecker == null) {
+                        Move move = new Move(from, destination, 1);
+                        res.add(move);
+                    }
                 }
             }
         }
@@ -190,37 +237,31 @@ public class CheckersBotImpl implements CheckersBot {
 
     private void checkPosAndAddToList(List<Checker> checkers, String teamColor,
                                       List<Move> list, int from,
-                                      int to, int secondTo) {
+                                      int to) {
 
-        int checkedPos = checkPos(from, to, teamColor, checkers);
+        int checkedPos = checkPos(to, teamColor, checkers);
 
         if (checkedPos != -1) {
             int priority = checkedPos == to ? 0 : 1;
-            Move theSecondMove = new Move(from, secondTo, priority);
+            Move theSecondMove = new Move(from, to, priority);
             list.add(theSecondMove);
         }
     }
 
-    private int checkPos(int from, int to, String teamColor, List<Checker> checkers) {
+    private int checkPos(int to, String teamColor, List<Checker> checkers) {
 
         Checker checkerTo = findCheckerByPosition(to, checkers);
 
         if (checkerTo == null) {
             return to;
-        } else if (checkerTo.getPosition() == to && checkerTo.getColor().equals(teamColor)) {
+        } else if (checkerTo.getColor().equals(teamColor)) {
             return -1;
-        } else if (checkerTo.getPosition() != to && !checkerTo.getColor().equals(teamColor)) {
-            return checkAndGetPosAfterKill(from, to, teamColor, checkers);
+        } else if (!checkerTo.getColor().equals(teamColor)) {
+            return -1;
         }
 
 
         return to;
-    }
-
-    private int checkAndGetPosAfterKill(int from, int to, String teamColor, List<Checker> checkers) {
-
-
-        return -1;
     }
 
     private Checker findCheckerByPosition(int to, List<Checker> checkers) {
